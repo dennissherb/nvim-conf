16f0082a850364d36ffb84c1147699339c0b5d2f
dennis <qkweint@gmail.com>
2024-03-17 21:06:27 +0200

    mapped runcode and fix

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,12 @@
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 vim.g.mapleader = ' '
+
+map('n', '<C-b>', ':lua vim.lsp.buf.code_action()<CR>')
+map('n', '<leader>r', ':RunCode<CR>')
